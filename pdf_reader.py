3761119dbc6d966f24e3f52084545fb1d4364da5
kanutanwarhero-gif <kanu.tanwar.hero@gmail.com>
2026-06-29 23:01:59 +0530

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,1 +1,18 @@
+import fitz
+from PIL import Image
+import io
 
+def pdf_to_images(pdf_path):
+    doc = fitz.open(pdf_path)
+
+    images = []
+
+    for page in doc:
+
+        pix = page.get_pixmap(matrix=fitz.Matrix(3,3))
+
+        img = Image.open(io.BytesIO(pix.tobytes("png")))
+
+        images.append(img)
+
+    return images
